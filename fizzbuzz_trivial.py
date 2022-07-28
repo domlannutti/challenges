30fd01c4bf9066b190e3b97d0c7011a7274041f5
Dominic Lannutti <34784127+domlannutti@users.noreply.github.com>
2022-07-28 17:45:49 -0500

    Update fizzbuzz_trivial.py
    Fixed function annotation

--- fizzbuzz_trivial.py
+++ fizzbuzz_trivial.py
@@ -1,28 +1,27 @@
 # Author: Dominic Lannutti
 # Date: 31 January 2022
 
-def main(lim: int) -> int:
+def main(lim: int) -> None:
     # exit function if given limit is not an integer
     if lim % 1 != 0:
-        return False
+        return None
    
    # exit function if given limit is less than 0
     elif lim < 0:
-        return False
+        return None
 
     # create generator loop using modulus operators to find fizziness and buzziness
     for i in range(1, lim+1):
         if i % 15 == 0:
-            yield "fizzbuzz"
+            print("fizzbuzz")
         elif i % 3 == 0:
-            yield "fizz"
+            print("fizz")
         elif i % 5 == 0:
-            yield "buzz"
+            print("buzz")
         else:
-            yield i
+            print(i)
 
 
 if __name__ == "__main__":
     lim = 100
-    for n in main(lim):
-        print(n)
+    main(lim)
